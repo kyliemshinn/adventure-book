624fe3fbb1573d934f006d2c6e9ead7ab875a381
John Netzel <johnrnetzel@gmail.com>
2022-06-06 15:33:53 -0800

    Add constraints to User model

--- server/models/User.js
+++ server/models/User.js
@@ -3,13 +3,20 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
   {
     username: {
-      type: String
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
     email: {
-      type: String
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address']
     },
     password: {
-      type: String
+      type: String,
+      required: true
     }
   }
 );
