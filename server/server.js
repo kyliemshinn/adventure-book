454c7274d6a51d3dcebe8542543b84723901be64
John Netzel <johnrnetzel@gmail.com>
2022-06-05 16:08:10 -0800

    Add ability for server to serve client assets when in production

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//app.use(routes);
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 db.once('open', () => {
     app.listen(PORT, () => {
