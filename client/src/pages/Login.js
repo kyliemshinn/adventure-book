861800f017e2202cb5c9e82026b81b81b7e7628b
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-05 19:53:52 -0700

    center signup and login forms

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,27 +3,29 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h1 className="card-title">Account Login</h1>
-        <h2>Username</h2>
-        <input
-          type="username"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <h2>Password</h2>
-        <input
-          type="password"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <div className="card-actions justify-center">
-          <button className="btn btn-primary">Login</button>
-          {/* TODO: add in once router is set in App.js with page routes */}
-        {/* <Link to="Signup">
+    <div class="justify-center flex ml-4 mr-4">
+      <div className="card w-96 bg-base-100 shadow-xl m-36">
+        <div className="card-body">
+          <h1 className="card-title">Account Login</h1>
+          <h2>Username</h2>
+          <input
+            type="username"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <h2>Password</h2>
+          <input
+            type="password"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Login</button>
+            {/* TODO: add in once router is set in App.js with page routes */}
+            {/* <Link to="Signup">
             <p>Sign Up Today!</p>
-        </Link> */}
+            </Link> */}
+          </div>
         </div>
       </div>
     </div>
