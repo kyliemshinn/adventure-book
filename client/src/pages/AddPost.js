245258cd75148b9eba8bae88a5054d9ef92d8c20
John Netzel <johnrnetzel@gmail.com>
2022-06-05 18:43:31 -0800

    Move map component to add post page

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { ReactBingmaps } from 'react-bingmaps-plus';
+
 const AddPost = () => {
     return (
         <div>
-            
+            <div style={{ height: "600px", width: "800px"}}>
+                <ReactBingmaps bingmapKey="AuobAMXGIQwgjimas4B-M6-ohLbmLaLNDIUojn2nI-VCDEh1VxaL__j48GUmEu-C" key={"A"}/>
+            </div>
         </div>
     )
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { Hero, Card, Button } from "react-daisyui";
 //import '../App.css';
-import { ReactBingmaps } from 'react-bingmaps-plus';
 
 const Home = () => {
   return (
     <div>
-      <div style={{ height: "600px", width: "800px"}}>
-        <ReactBingmaps bingmapKey="AuobAMXGIQwgjimas4B-M6-ohLbmLaLNDIUojn2nI-VCDEh1VxaL__j48GUmEu-C" key={"A"}/>
-      </div>
       <Hero className="min-h-screen bg-base-100">
         
         <Hero.Content className="text-center">
