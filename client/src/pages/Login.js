d2e014401acb8b71e90217ba7ffedaa24ba723f7
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-07 12:08:42 -0700

    add the styling of the input field for the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,31 +1,34 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
+import "../styles/Login.css";
 
 const Login = () => {
   return (
     <div className="justify-center flex ml-4 mr-4">
-      <div className="card w-96 bg-base-100 shadow-xl m-36">
+      <div className="card w-96 shadow-xl m-36">
         <div className="card-body">
-          <h1 className="card-title">Account Login</h1>
-          <h2 className="text-secondary-content">Username</h2>
-          <input
-            type="username"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-          <h2>Password</h2>
-          <input
-            type="password"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
+          <h1 className="card-title p-4">WELCOME BACK</h1>
+          <div className="input-icons">
+            <i className="fa-solid fa-user text-secondary-content icon"></i>
+            <input
+              type="username"
+              placeholder="Username"
+              className="input input-bordered w-full max-w-xs input-field text-secondary-content"
+            />
+            <i class="fa-solid fa-lock text-secondary-content icon"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full max-w-xs input-field text-secondary-content"
+            />
+          </div>
           <div className="card-actions justify-center">
             <button className="btn btn-primary">Login</button>
-            {/* TODO: add in once router is set in App.js with page routes */}
           </div>
           <div className="justify-center ">
             <Link to="/signup">
-            <h3 className="text-secondary-content p-4">Sign Up Today!</h3>
+              <h3 className="text-secondary-content p-4">Sign Up Today!</h3>
             </Link>
           </div>
         </div>
