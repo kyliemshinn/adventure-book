2d18188231c33712aebd2a2144349f26126ab138
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-07 20:48:27 -0700

    refactor addPost file code to use cloudinary for picture upload

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -5,48 +5,55 @@ import { CREATE_POST } from "../utils/mutation";
 // import Button from "../components/MainButton/Button"
 
 function AddPost() {
-  const [selectedFile, setSelectedFile] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
-  const [createPost, setPost] = useState({content:"", author:"Kylie", tags:[""], location:{latitude:0, longitude:0}});
 
-  const changeHandler = (event) => {
-    setSelectedFile(event.target.files[0]);
-    setIsFilePicked(true);
-  };
-
-  const handleSubmission = () => {
-    const formData = new FormData();
-
-    formData.append("File", selectedFile);
-
-    //TODO: change route according to what we will need
-    fetch("/addpost/", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        console.log("Success:", result);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
-
-  const [addPost, { error }] = useMutation(CREATE_POST);
-
-  const handlePostSubmit = async (event) => {
+  //setting up upload image
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+  
+  const uploadImage = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload-preset", "qufwrwdm");
+    setLoading(true);
+    const response = await fetch(
+      "https://api.cloudinary.com/v1_1/dw5epcgjt/image/upload",
+      {
+        method: "post",
+        body: data,
+      }
+      );
+      const file = await response.json();
+      setImage(file.secure_url);
+      setLoading(false);
+    };
+    
+    // when the button is clicked for new post to create - run this function
+    const [createPost, setPost] = useState({
+      content: "",
+      author: "Kylie",
+      tags: [""],
+      location: { latitude: 0, longitude: 0 },
+    });
+    const [addPost, { error }] = useMutation(CREATE_POST);
+    
+    const handlePostSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const data = await addPost({
-        variables: { content: createPost.content, author: createPost.author, tags: createPost.tags, location: createPost.location },
+        variables: {
+          content: createPost.content,
+          author: createPost.author,
+          tags: createPost.tags,
+          location: createPost.location,
+        },
       });
-      console.log(data)
+      console.log(data);
       setPost("");
     } catch (error) {
       console.error(error);
-      console.log("is this working")
+      console.log("testing");
     }
   };
 
@@ -58,7 +65,6 @@ function AddPost() {
             <h1 className="card-title justify-center text-secondary-content">
               Add A Post!
             </h1>
-            {/* {Auth.loggedIn() ? ( */}
             <div className="card bg-primary text-primary-content justify-center">
               <div className="card-body m-16">
                 <h2 className="card-title justify-center">Add Photo</h2>
@@ -67,26 +73,16 @@ function AddPost() {
                   <input
                     type="file"
                     name="file"
-                    onChange={changeHandler}
+                    placeholder="add image"
+                    onChange={uploadImage}
                     className="justify-center"
                   />
-                  {isFilePicked ? (
-                    <div>
-                      <img src={selectedFile} alt="selected"></img>
-                      <p>Filetype: {selectedFile.type}</p>
-                      <p>Size in bytes: {selectedFile.size}</p>
-                      <p>
-                        lastModifiedDate:{" "}
-                        {selectedFile.lastModifiedDate.toLocaleDateString()}
-                      </p>
-                    </div>
-                  ) : (
-                    <p>Select a file to show details</p>
-                  )}
-                  <div>
-                    <button onClick={handleSubmission}>Submit</button>
-                  </div>
                 </div>
+                {loading ? (
+                  <h3>Upload Image</h3>
+                ) : (
+                  <img src={image} style={{ width: "300px" }} alt="selected" />
+                )}
                 <textarea
                   className="textarea textarea-bordered"
                   placeholder="Description of where you went activities, restaurants..."
@@ -106,11 +102,6 @@ function AddPost() {
                 </div>
               </div>
             </div>
-            {/* ) : (
-              <p>
-                You need to be logged in to endorse skills. Please{' '}
-                <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-              </p> */}
           </div>
         </div>
       </div>
