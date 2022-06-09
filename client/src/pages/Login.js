9ca28d64a7dc4d0aa0b0910d956594ad29fa37bb
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-08 21:37:03 -0700

    add balloons feature to login in page on page load- will change to on click of user sign in

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN } from "../utils/mutation";
 import Auth from "../utils/auth";
-import { ReactFloatingBalloons } from "react-floating-balloons";
-
+import { Button } from "react-daisyui";
 import "font-awesome/css/font-awesome.min.css";
 import "../styles/Login.css";
-import { Button } from "react-daisyui";
+import "../styles/Balloons.css";
 
 const Login = () => {
-  const [loginState, setLoginState] = useState({ email: "", password: "" });
+  const [loginState, setLoginState] = useState({ username: "", password: "" });
 
   const [loginUser, { error, data }] = useMutation(LOGIN);
 
@@ -56,66 +55,100 @@ const Login = () => {
     setLoginState({ username: "", password: "" });
   };
 
-  return (
-    <div className="justify-center flex ml-4 mr-4">
-      <div className="card w-96 bg-base-100 shadow-xl m-36">
-        <div className="card-body p-16">
-          <h1 className="card-title p-4 justify-center text-secondary-content">
-            WELCOME BACK
-          </h1>
-          {data ? (
-            <p>
-              Success! You may now head{" "}
-              <Link to="/">back to the homepage.</Link>
-            </p>
-          ) : (
-            <form onSubmit={handleFormSubmit}>
-              <div className="input-icons">
-                <i className="fa-solid fa-user text-secondary-content icon"></i>
-                <input
-                  name="email"
-                  value={loginState.username}
-                  placeholder="Email"
-                  className="input input-bordered w-full max-w-xs input-field text-secondary-content"
-                  onChange={handleChange}
-                />
-                <i className="fa-solid fa-lock text-secondary-content icon"></i>
-                <input
-                  name="password"
-                  value={loginState.password}
-                  type="password"
-                  placeholder="Password"
-                  className="input input-bordered w-full max-w-xs input-field text-secondary-content"
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="card-actions justify-center text-center">
-                <Button
-                  className="btn btn-primary rounded-full bg-accent hover:bg-accent-focus hover:shadow-lg border-none text-base-content"
-                  type="submit"
-                >
-                  Login
-                </Button>
-              </div>
-            </form>
-          )}
-          {error && (
-            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
-          )}
+  function random(num) {
+    return Math.floor(Math.random() * num);
+  }
 
-          <div className="text-center p-4">
-            <h3 className="text-secondary-content text-lg">
-              Don't have an account?{" "}
-            </h3>
-            <Link to="/signup">
-              <h3 className="text-neutral text-lg">Sign Up!</h3>
-            </Link>
-            <ReactFloatingBalloons
-              count={10}
-              msgText="youre now logged in"
-              colors={["yellow", "purple", "red", "blue", "green"]}
-              popVolumeLevel={0.1}
-            />
+  function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5) + 5;
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+    color: rgba(${r},${g},${b},0.7); 
+    box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+    margin: ${mt}px 0 0 ${ml}px;
+    animation: float ${dur}s ease-in infinite
+    `;
+  }
+
+  function createBalloons(num) {
+    var balloonContainer = document.getElementById("balloon-container");
+    for (var i = num; i > 0; i--) {
+      var balloon = document.createElement("div");
+      balloon.className = "balloon";
+      balloon.style.cssText = getRandomStyles();
+      balloonContainer.append(balloon);
+    }
+  }
+
+  window.onload = function () {
+    createBalloons(100);
+  };
+
+  return (
+    <div id="balloon-container">
+      <div className="justify-center flex mr-4" id="loginForm">
+        <div className="card w-96 bg-base-200 shadow-xl m-36" id="loginForm">
+          <div className="card-body p-16">
+            <h1 className="card-title p-4 justify-center text-secondary-content">
+              WELCOME BACK
+            </h1>
+            {data ? (
+              <p>
+                Success! You may now head{" "}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              
+                <form onSubmit={handleFormSubmit}>
+                  <div className="input-icons">
+                    <i className="fa-solid fa-user text-secondary-content icon"></i>
+                    <input
+                      name="username"
+                      value={loginState.username}
+                      placeholder="Username"
+                      className="input input-bordered w-full max-w-xs input-field text-secondary-content"
+                      onChange={handleChange}
+                    />
+                    <i className="fa-solid fa-lock text-secondary-content icon"></i>
+                    <input
+                      name="password"
+                      value={loginState.password}
+                      type="password"
+                      placeholder="Password"
+                      className="input input-bordered w-full max-w-xs input-field text-secondary-content"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="card-actions justify-center text-center">
+                    <Button
+                      className="btn btn-primary rounded-full bg-accent hover:bg-accent-focus hover:shadow-lg border-none text-base-content"
+                      type="submit"
+                    >
+                      Login
+                    </Button>
+                  </div>
+                </form>
+             
+            )}
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+
+            <div className="text-center p-4">
+              <h3 className="text-secondary-content text-lg">
+                Don't have an account?{" "}
+              </h3>
+              <Link to="/signup">
+                <h3 className="text-neutral text-lg">Sign Up!</h3>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
