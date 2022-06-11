a8b65219e3ae5216bfb3fa460212c6ae20ab2590
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-11 14:45:05 -0700

    change styling to add post to get card to be in columns

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { CREATE_POST } from "../utils/mutation";
-import Map from "../components/Map"
+import Map from "../components/Map";
 import { Link } from "react-router-dom";
-
 // import Button from "../components/MainButton/Button"
 
 function AddPost() {
-
   //setting up upload image
   const [images, setImage] = useState("");
   const [loading, setLoading] = useState(false);
@@ -22,7 +20,7 @@ function AddPost() {
       "https://api.cloudinary.com/v1_1/dw5epcgjt/image/upload",
       {
         method: "post",
-        body: data,
+        body: data
       }
     );
     const file = await res.json();
@@ -41,8 +39,7 @@ function AddPost() {
 
   const [location, setLocation] = useState([0.01, 0.01]);
 
-  function readLocation(arg)
-  {
+  function readLocation(arg) {
     /*setPushPins([{
       "location":[arg.latitude, arg.longitude],
       "option":{ color: 'red' }
@@ -56,103 +53,127 @@ function AddPost() {
   const handlePostSubmit = async (event) => {
     event.preventDefault();
 
-    const processedLocation = { latitude: location[0], longitude: location[1] }
+    const processedLocation = { latitude: location[0], longitude: location[1] };
     console.log(createPost);
 
     try {
-    await addPost({
-      variables: {
-        images: [images],
-        title: createPost.title,
-        content: createPost.content,
-        tags: createPost.tags,
-        location: processedLocation,
-      },
-    });
-    window.location.assign('/dashboard');
-    } catch(e) {
+      await addPost({
+        variables: {
+          images: [images],
+          title: createPost.title,
+          content: createPost.content,
+          tags: createPost.tags,
+          location: processedLocation
+        }
+      });
+      window.location.assign("/dashboard");
+    } catch (e) {
       alert(e);
     }
   };
 
   function handleChange(e) {
     let { name, value } = e.target;
-    if(name === "tags") {
+    if (name === "tags") {
       value = value.split(" ");
     }
     setPost({
       ...createPost,
-      [name]: value,
+      [name]: value
     });
   }
   return (
     <div className="pageContainer">
       <div>
-        <div className="card lg:card-side bg-base-100 shadow-xl m-24">
-          <div className="card-body">
+        <div className="card lg:card-side bg-base-100 shadow-xl ">
+          <div className="card-body ">
+            <div className="card bg-primary text-primary-content justify-center ">
             <h1 className="card-title justify-center text-secondary-content text-3xl p-4">
               Add A Post!
             </h1>
-            <div className="card bg-primary text-primary-content justify-center">
-              <div className="card-body m-16 justify-center">
-                <h2 className="card-title justify-center">Add Photo</h2>
-                <input
-                  type="file"
-                  name="file"
-                  multiple="multiple"
-                  accept='image/jpeg, image/png, image/jpg'
-                  placeholder="add image"
-                  onChange={uploadImage}
-                  className="place-items-center"
-                />
-                {loading ? (
-                  <>
-                  <h3>Uploading Image...</h3>
-                  </>
-                ) : (
-                  <>
-                  <div className='grid row'>
-                  <img src={images} style={{ width: "300px" }} alt="selected" />
+              <div className="card-body mt-20 justify-center grid grid-cols-2">
+                <div className="mt-32 mx-10">
+                  {/* <h2 className="card-title">Add Photo</h2> */}
+                  <div className="m-2">
+                  <input
+                    type="file"
+                    name="file"
+                    multiple="multiple"
+                    accept="image/jpeg, image/png, image/jpg"
+                    placeholder="add image"
+                    onChange={uploadImage}
+                    className="place-items-center" 
+                  />
+                  {loading ? (
+                    <>
+                      <h3>Uploading Image...</h3>
+                    </>
+                  ) : (
+                    <>
+                      <div className="grid row">
+                        <img
+                          src={images}
+                          style={{ width: "300px" }}
+                          alt="selected"
+                        />
+                      </div>
+                    </>
+                  )}
                   </div>
-                  </>
-                )}
-                <input
-                  type="text"
-                  name="title"
-                  placeholder="Title"
-                  className="input input-bordered"
-                  onChange={handleChange}
-                  onSubmit={() => {console.log("!!!")}}
-                />
-                <input
-                  type="text"
-                  name="tags"
-                  placeholder="#Tags"
-                  className="input input-bordered"
-                  onChange={handleChange}
-                />
-                <textarea
-                  name="content"
-                  className="textarea textarea-bordered"
-                  placeholder="Description of where you went activities, restaurants..."
-                  onChange={handleChange}
-                ></textarea>
-                <Map height="600px" width="800px" locations={[pushPins]} onClick={readLocation}/>
-                <div>
-                  <p>Latitude: {location[0]}</p>
-                  <p>Longitude: {location[1]}</p>
+                  <div className="m-3">
+                  <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    className="input input-bordered w-full"
+                    onChange={handleChange}
+                    onSubmit={() => {
+                      console.log("!!!");
+                    }}
+                  />
+                  </div>
+                  <div className="m-3">
+                  <input
+                    type="text"
+                    name="tags"
+                    placeholder="#Tags"
+                    className="input input-bordered w-full"
+                    onChange={handleChange}
+                  />
+                  </div>
+                  <div className="m-3">
+                  <textarea
+                    name="content"
+                    className="textarea textarea-bordered w-full"
+                    placeholder="Description of where you went activities, restaurants..."
+                    onChange={handleChange}
+                  ></textarea>
+                  </div>
                 </div>
-                <div className="card-actions justify-end">
-                  <Link to="/dashboard">
-                    <button
-                      onClick={handlePostSubmit}
-                      className="btn btn-primary rounded-full"
-                    >
-                      ADD POST
-                    </button>
-                  </Link>
+                <div className="ml-16">
+                  <h3>Click On The Map To Pin A Location</h3>
+                  <Map
+                    height="480px"
+                    width="650px"
+                    locations={[pushPins]}
+                    onClick={readLocation}
+                  />
+                  <div>
+                    <p>Latitude: {location[0]}</p>
+                    <p>Longitude: {location[1]}</p>
+                  </div>
                 </div>
               </div>
+              <div className="card-actions justify-end m-6">
+                    <Link to="/dashboard">
+                      <button
+                        onClick={handlePostSubmit}
+                        className="btn btn-primary rounded-full"
+                      >
+                        ADD POST
+                      </button>
+                    </Link>
+                  </div>
             </div>
           </div>
         </div>
--- client/src/pages/EditPost/index.js
+++ client/src/pages/EditPost/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { Hero } from "react-daisyui";
+import React, { useState } from "react";
+// import { Hero } from "react-daisyui";
 import { UPDATE_POST } from "../../utils/mutation";
 import { useMutation } from "@apollo/client";
 import { useParams } from "react-router-dom";
@@ -7,78 +7,77 @@ import "../../App.css";
 
 const EditPost = ({ title, tags, content }) => {
   const params = useParams();
-  const [ postState, setPostState ] = useState({});
+  const [postState, setPostState] = useState({});
 
   const handleChange = (e) => {
     let { name, value } = e.target;
-    if(name === "tags") {
+    if (name === "tags") {
       value = value.split(" ");
     }
     setPostState({
       ...postState,
-      [name]: value,
+      [name]: value
     });
-  }
+  };
   const [updatePost] = useMutation(UPDATE_POST);
 
- const handleEditSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    console.log(params.postId)
-    const updatedPost = await updatePost({
-      variables: {
-        postId: params.postId,
-        title: postState.title,
-        tags: postState.tags,
-        content: postState.content
-      }
-    });
-    console.log(updatedPost.data.updatePost.id)
-    window.location.assign('/dashboard');
-  } catch (err) {
-    console.error(err)
-  }
-
- }
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(params.postId);
+      const updatedPost = await updatePost({
+        variables: {
+          postId: params.postId,
+          title: postState.title,
+          tags: postState.tags,
+          content: postState.content
+        }
+      });
+      console.log(updatedPost.data.updatePost.id);
+      window.location.assign("/dashboard");
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="pageContainer">
-    <div>
-      <div className="card lg:card-side bg-base-100 shadow-xl m-24">
-        <div className="card-body">
-          <h1 className="card-title justify-center text-secondary-content text-3xl p-4">
-            Update A Post!
-          </h1>
-          <div className="card bg-primary text-primary-content justify-center">
-            <div className="card-body m-16 justify-center">
-              <input
-                type="text"
-                name="title"
-                placeholder="Title"
-                className="input input-bordered"
-                value={title}
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="tags"
-                placeholder="#Tags"
-                className="input input-bordered"
-                value={tags}
-                onChange={handleChange}
-              />
-              <textarea
-                name="content"
-                className="textarea textarea-bordered"
-                placeholder="Update Descripton"
-                value={content}
-                onChange={handleChange}
-              ></textarea>
-                  <button
-                    onClick={handleEditSubmit}
-                    className="btn btn-primary rounded-full"
-                  >
-                    Update POST
-                  </button>
+      <div>
+        <div className="card lg:card-side bg-base-100 shadow-xl m-24">
+          <div className="card-body">
+            <h1 className="card-title justify-center text-secondary-content text-3xl p-4">
+              Update A Post!
+            </h1>
+            <div className="card bg-primary text-primary-content justify-center">
+              <div className="card-body m-16 justify-center">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Title"
+                  className="input input-bordered"
+                  value={title}
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="tags"
+                  placeholder="#Tags"
+                  className="input input-bordered"
+                  value={tags}
+                  onChange={handleChange}
+                />
+                <textarea
+                  name="content"
+                  className="textarea textarea-bordered"
+                  placeholder="Update Descripton"
+                  value={content}
+                  onChange={handleChange}
+                ></textarea>
+                <button
+                  onClick={handleEditSubmit}
+                  className="btn btn-primary rounded-full"
+                >
+                  Update POST
+                </button>
               </div>
             </div>
           </div>
