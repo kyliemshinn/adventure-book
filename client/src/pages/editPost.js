b4956f89b974146c5993c59744b88e9cd2dd2ae1
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-10 18:27:34 -0700

    add update post styling for input fields

--- client/src/pages/editPost.js
+++ client/src/pages/editPost.js
@@ -7,32 +7,46 @@ const EditPost = () => {
 
 
   return (
-    <div className="text-neutral-content">
-      <Hero className="mb-28">
-        <div className="postContainer bg-base-200 mx-9 my-9 pt-4 px-6 pb-7">
-          <div>
-            <div>
-             <span className="bookmark"> <i className="fa-solid fa-bookmark fa-xl pt-3 float-right" title="save post"></i></span>
-            </div>
-            <h2 className="text-bold text-2xl text-neutral-content">
-            </h2>
-          </div>
-          <div className="grid grid-cols-2 gap-4 py-3 pb-9">
-            <div className="slider h-vh">
-              <div className="badge badge-outline"></div>
-            </div>
-
-            <div className="bg-base-100 mb-6 ">
-              <p className="float-right"></p>
-              <div className="max-w-2xl">
-                <h3 className="pl-8 pt-5 overflow-wrap"> </h3>
+    <div className="pageContainer">
+    <div>
+      <div className="card lg:card-side bg-base-100 shadow-xl m-24">
+        <div className="card-body">
+          <h1 className="card-title justify-center text-secondary-content text-3xl p-4">
+            Update A Post!
+          </h1>
+          <div className="card bg-primary text-primary-content justify-center">
+            <div className="card-body m-16 justify-center">
+              <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                className="input input-bordered"
+                // onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="tags"
+                placeholder="#Tags"
+                className="input input-bordered"
+                // onChange={handleChange}
+              />
+              <textarea
+                name="content"
+                className="textarea textarea-bordered"
+                placeholder="Update Descripton"
+                // onChange={handleChange}
+              ></textarea>
+                  <button
+                    // onClick={handlePostSubmit}
+                    className="btn btn-primary rounded-full"
+                  >
+                    Update POST
+                  </button>
               </div>
             </div>
           </div>
-          <div className="bg-base-100 px-8">
-          </div>
         </div>
-      </Hero>
+      </div>
     </div>
   );
 };
