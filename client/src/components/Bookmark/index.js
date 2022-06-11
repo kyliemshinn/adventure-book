a19cc009bc8c05658ef4e2bd3987d97d6aaf59ec
Miacarmen <maitreyacarmen@gmail.com>
2022-06-10 20:25:26 -0700

    bookmark color toggle implemented, locations added to postSeed

--- client/src/components/Bookmark/index.js
+++ client/src/components/Bookmark/index.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+// import React from 'react';
+import { BsFillBookmarkHeartFill } from "react-icons/bs";
+import axios from "axios";
+
 
 const Bookmark = () => {
-    return (
-        <div>
-            <button className="bookmark float-right">
-                <i
-                  className="fa-solid fa-bookmark fa-xl pt-3"
-                  title="save post"
-                ></i>
-              </button>
-        </div>
-    )
-}
+  // set toggle state to false
+  const [colorToggle, setColorToggle] = useState('black');
+//   const [favList, setFavList] = useState([]);
+
+// const handleClick = (event) => {
+//     console.log('button clicked');
+//     event.target.style.color = "red";
+//     console.log(event.target);
+// }
+
+// const addToFaveList = () => {
+    // .GET favorite collection
+    // if current post is already saved, do nothing
+
+    // .POST current post data to collection
+// }
+
+  return (
+    <div>
+      <div className="bookmark">
+        {/* if post is NOT favorited, display as Black */}
+        
+          <BsFillBookmarkHeartFill
+            className="float-right"
+            onClick={() => {setColorToggle("red")}}
+            style={{ color: colorToggle }}
+            size={30}
+          />
+       
+      </div>
+    </div>
+  );
+};
 
 export default Bookmark;
+
