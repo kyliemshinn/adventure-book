942e0482d8abe9fbd046eb67825f694af84ae982
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-11 18:59:41 -0700

    add media queries to login style sheet ot make login/signup mobile responsive

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -87,8 +87,8 @@ function displayBalloons () {
 
   return (
     <div id="balloon-container">
-      <div className="justify-center mr-4" id="loginForm">
-        <div className="card w-96 bg-base-200 shadow-xl m-36" id="loginForm">
+      <div className="justify-center mr-4 loginForm">
+        <div className="card w-96 bg-base-200 shadow-xl m-36 loginForm">
           <div className="card-body p-16">
             <h1 className="card-title p-4 justify-center text-secondary-content">
               WELCOME BACK
