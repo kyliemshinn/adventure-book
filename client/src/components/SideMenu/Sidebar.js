ec8b6a8a70556c0ea7ae8f2e86b3dc284cfa644a
Kylie Shinn <kyliemshinn@gmail.com>
2022-06-11 22:20:35 -0700

    hard code data for saved posts on dashboard

--- client/src/components/SideMenu/Sidebar.js
+++ client/src/components/SideMenu/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import '../../styles/Dashboard.css'
+import '../../styles/Dashboard.css';
+import { Badge } from "react-daisyui";
 
 const Sidebar = () => {
   return (
@@ -10,13 +11,37 @@ const Sidebar = () => {
         </div>
         <div className="place-items-center">
         <div className="pl-3 pt-3">
-          {/* <div className="card w-96 h-36 bg-base-100 shadow-xl savePost">
-            <div className="card-body">
-              <h2 className="card-title">Post Title</h2>
-              <p>Username</p>
-              <p>#Tags</p>
+
+          {/* saved cards */}
+          <div className="card w-96 h-36 bg-base-100 shadow-xl savePost m-3">
+            <div className="card-body p-5">
+              <h2 className="card-title">Italy</h2>
+              <p>Carl Fredricksen</p>
+              <Badge className="badge badge-outline">#italy</Badge>
+              <Badge className="badge badge-outline">#colosseum</Badge>
             </div>
-          </div> */}
+          </div>
+
+          <div className="card w-96 h-36 bg-base-100 shadow-xl savePost m-3">
+            <div className="card-body p-5">
+              <h2 className="card-title">Bali</h2>
+              <p>Carl Fredricksen</p>
+              <Badge className="badge badge-outline">#bali</Badge>
+              <Badge className="badge badge-outline">#jimbaran</Badge>
+              <Badge className="badge badge-outline">#spa</Badge>
+            </div>
+          </div>
+
+          <div className="card w-96 h-36 bg-base-100 shadow-xl savePost m-3">
+            <div className="card-body p-5">
+              <h2 className="card-title">Dijbouti</h2>
+              <p>Carl Fredricksen</p>
+              <Badge className="badge badge-outline">#redsea</Badge>
+              <Badge className="badge badge-outline">#dijbouti</Badge>
+              <Badge className="badge badge-outline">#whitesaltbeaches</Badge>
+            </div>
+          </div>
+
           </div>
         </div>
       </div>
